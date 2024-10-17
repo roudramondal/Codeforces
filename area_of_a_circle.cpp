22fc11d1be767910b8b61e93c272dc5bd8aa80d9
Roudra <103403881+roudramondal@users.noreply.github.com>
2024-10-16 22:27:24 -0600

    Add files via upload

---
+++ area_of_a_circle.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main ()
+{
+  double r,area;
+  cin >> r;
+  area = 3.141592653*(r*r);
+  cout << fixed << setprecision(9)<<area<<endl;
+  return 0;
+}
---
+++ calculator.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  long long x, y;
+  cin >> x >> y;
+  // int sum1 = x + y;
+  // int sum2 = x * y;9
+  // int sum3 = x - y;
+
+  cout << x<<" "<<"+"<<" "<< y<<" " << "=" << " "<<x+y<<endl;
+  cout << x<<" "<<"*"<<" "<< y<<" " << "="<<" "<<x*y<<endl;
+  cout << x<<" "<<"-"<<" "<< y<<" " << "="<<" "<<x-y<<endl;
+
+  return 0;
+}
