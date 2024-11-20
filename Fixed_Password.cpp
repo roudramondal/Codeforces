78f46ec893a11560f5a3e963a54e42d2f19d7d23
Roudra <103403881+roudramondal@users.noreply.github.com>
2024-11-20 22:44:48 +0600

    Add files via upload
    loop sheet#2

---
+++ Fixed_Password.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int n;
+  while (true)
+  {
+    cin >> n;
+    if (n==1999)
+   {
+     cout << "Correct" << endl;
+      break;
+   }
+    else
+    {
+    cout << "Wrong" << endl;
+   
+    }
+    
+  }
+  
+  
+  return 0;
+}
